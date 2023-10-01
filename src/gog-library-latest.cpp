63e742f35a342e7c96f4c7df3a1c5db659b2d6ed
Andrew 'The White Satchel Paige' Martin <rockiesmagicnumber@gmail.com>
2023-09-30 23:14:01 -0600

    added launch.json, continue writing parsing fxns

--- src/gog-library-latest.cpp
+++ src/gog-library-latest.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <pstream.h>
 #include <iostream>
 #include <string>
@@ -9,27 +10,62 @@
 #include "headers.h"
 
 using namespace std;
+using namespace constants;
+using namespace constants::lgogdownloader;
+using namespace goglibrarylatest;
 
 // main
 int main()
 {
+	// get all directories in installer root
 	string allDirectoriesCommand = lsCommand + space + gameInstallersRoot;
 	const char *allDirectoriesCharCommand = allDirectoriesCommand.c_str();
 	string allDirectoriesStr = execCLI(allDirectoriesCharCommand);
 
-	std::vector<std::string> allDirectories = split(allDirectoriesStr, newlineDelimiter);
-	for (int i = 0; i < allDirectories.size(); i++)
+	// get all directories from console return
+	vector<string> allDirectories = split(allDirectoriesStr, newlineDelimiter);
+
+	// iterate over each game directory
+	for (size_t gameDirIndex = 20;
+		 gameDirIndex < allDirectories.size();
+		 gameDirIndex++)
 	{
-		string currentDirectory = allDirectories[i];
+		string currentDirectory = allDirectories[gameDirIndex];
+		string currentGame = currentDirectory.substr(0, currentDirectory.size() - 1);
 		if (currentDirectory.empty())
 		{
 			continue;
 		}
-		// std::cout << currentDirectory << std::endl;
-		string fullCommand = lsCommand + space + gameInstallersRoot + currentDirectory + gogDir + space + pipeGrepComand;
-		const char *fullCharCommand = fullCommand.c_str();
+		// get all installer files in the game directory
+		// cout << currentDirectory << endl;
+		string currentInstallersCommand = lsCommand + space + gameInstallersRoot + currentDirectory + gogDir + space + pipeGrepComand;
+		const char *currentInstallersCharCommand = currentInstallersCommand.c_str();
+		set<string> currentlyDownloadedInstallers = vectorToSet(splitCLIresults(currentInstallersCharCommand, newlineDelimiter));
 
-		string foo = execCLI(fullCharCommand);
-		std::vector<std::string> installerFiles = split(foo, newlineDelimiter);		
+		// get list of files from lgog
+		string thisGameCommand = lgogdownladerCommand + space + lgogdownloaderListDetailsArgument + space + lgogdownloaderGameArgument + space + currentGame;
+		// cout << thisGameCommand << endl;
+		const char *thisGameCharCommand = thisGameCommand.c_str();
+		map<string, string> installerFileNamesFromGOG = getInstallerFilesFromGOG(splitCLIresults(thisGameCharCommand, newlineDelimiter));
+
+		vector<string> filesToGetFromGOG;
+		for (pair<string, string> fn : installerFileNamesFromGOG)
+		{
+			// check to see if the current file from GOG already exists in the `currentlyDownloadedInstallers` set
+			//  if not, filesToGetFromGOG.push_back gamename/fileid
+			//  if so, continue
+		}
+		
+		// create comma-separated string of {gamename/fileid}s and pass it to the `--download-file` parameter in lgog
+
+
+		for (string existingFile : currentlyDownloadedInstallers)
+		{
+			// check each `existingFile` to ensure it's on the GOG list.
+			//  If so, continue
+			//  If not, delete
+		}
+
+		// for_each(installerFileNamesFromGOG.begin(), installerFileNamesFromGOG.end(), logthis);
 	}
 }
--- src/headers.h
+++ src/headers.h
@@ -1,3 +1,4 @@
+#include <map>
 #include <pstream.h>
 #include <iostream>
 #include <string>
@@ -6,45 +7,131 @@
 #include <memory>
 #include <stdexcept>
 #include <vector>
+#include <set>
 
-const std::string lsCommand("ls -p");
-const std::string pipeGrepComand("| grep -v /");
-const std::string gameInstallersRoot("/media/the-archive/Game-Installers/");
-const std::string gogDir("gog/");
-const std::string space(" ");
-const std::string newlineDelimiter("\n");
+using namespace std;
 
-// execute CLI function and return the output from stdout as a string - https://stackoverflow.com/a/478960
-std::string execCLI(const char *cmd)
+namespace constants
 {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe)
+    const string lsCommand("ls -p");
+    const string pipeGrepComand("| grep -v /");
+    const string gameInstallersRoot("/media/the-archive/Game-Installers/");
+    const string gogDir("gog/");
+    const string space(" ");
+    const string newlineDelimiter("\n");
+
+    namespace lgogdownloader
     {
-        throw std::runtime_error("popen() failed!");
+        const string lgogdownladerCommand("lgogdownloader --platform w+l");
+        const string lgogdownloaderDownloadArguments("--download --directory /media/the-archive/Game-Installers/ --subdir-installers gog");
+        const string lgogdownloaderGameArgument("--game");
+        const string lgogdownloaderListDetailsArgument("--list-details");
+        const string pathRowConstant("\tpath:");
+        const string fileIdRowConstant("\tid:");
     }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-    {
-        result += buffer.data();
-    }
-    return result;
 }
 
-// for string delimiter - taken directly from https://stackoverflow.com/a/46931770
-std::vector<std::string> split(std::string s, std::string delimiter)
+namespace goglibrarylatest
 {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    std::string token;
-    std::vector<std::string> res;
-
-    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos)
+    using namespace constants;
+    using namespace constants::lgogdownloader;
+    // execute CLI function and return the output from stdout as a string - https://stackoverflow.com/a/478960
+    string execCLI(const char *cmd)
     {
-        token = s.substr(pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back(token);
+        array<char, 128> buffer;
+        string result;
+        unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        if (!pipe)
+        {
+            throw runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+        {
+            result += buffer.data();
+        }
+        return result;
     }
 
-    res.push_back(s.substr(pos_start));
-    return res;
+    // for string delimiter - taken directly from https://stackoverflow.com/a/46931770
+    vector<string> split(string s, string delimiter)
+    {
+        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+        string token;
+        vector<string> res;
+
+        while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
+        {
+            token = s.substr(pos_start, pos_end - pos_start);
+            pos_start = pos_end + delim_len;
+            res.push_back(token);
+        }
+
+        res.push_back(s.substr(pos_start));
+        return res;
+    }
+
+    // convert a vector to a set
+    template <typename T>
+    set<T> vectorToSet(const vector<T> &inputVector)
+    {
+        set<string> s(inputVector.begin(), inputVector.end());
+        return s;
+    }
+
+    vector<string> splitCLIresults(const char *cmd, string delimiter)
+    {
+        return split(execCLI(cmd), delimiter);
+    }
+
+    map<string, string> getInstallerFilesFromGOG(vector<string> thisGameDetailList)
+    {
+        map<string, string> gogInstallers;
+        bool isInInstallersSection = false;
+        string fileId, path;
+        for (size_t t = 0; t < thisGameDetailList.size(); t++)
+        {
+            string curLine = thisGameDetailList[t];
+            if (curLine.rfind("installers", 0) == 0)
+            {
+                isInInstallersSection = true;
+                continue;
+            }
+            else if (!isInInstallersSection)
+            {
+                continue;
+            }
+            if (isInInstallersSection)
+            {
+                if (curLine.rfind(pathRowConstant, 0) == 0)
+                {
+                    string filename = curLine.substr(curLine.find_last_of("/") + 1);
+                    path = filename;
+                }
+                if (curLine.rfind(fileIdRowConstant, 0) == 0)
+                {
+                    string filename = curLine.substr(curLine.find_last_of(":") + 1);
+                    fileId = filename;
+                }
+            }
+            if (curLine.rfind("\t") == 0)
+            {
+                continue;
+            }
+            else
+            {
+                if (!fileId.empty() && !path.empty())
+                {
+                    gogInstallers[fileId] = path;
+                }
+
+                break;
+            }
+        }
+        return gogInstallers;
+    }
+
+    void logthis(string s)
+    {
+        cout << s << endl;
+    }
 }
